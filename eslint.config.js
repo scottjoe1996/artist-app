bbf99d42b8a19b9381205066ea06f73adcb605bb
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-06-08 16:13:42 +0100

    added a11y eslint package to es lint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,27 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
+      ...jsxA11y.flatConfigs.recommended,
     languageOptions: {
+      ...jsxA11y.flatConfigs.recommended.languageOptions,
       ecmaVersion: 2020,
       globals: globals.browser,
+
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      "jsx-a11y": jsxA11y
     },
     rules: {
+      ...jsxA11y.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
