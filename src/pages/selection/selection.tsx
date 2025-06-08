14d0ff4e9aaf968fcfd4b1349965cad9b917fd47
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-06-08 12:12:28 +0100

    created stubbed show lyrics section

--- src/pages/selection/selection.tsx
+++ src/pages/selection/selection.tsx
@@ -1,17 +1,37 @@
 import React from "react";
 
 import Title from "../../components/title";
+
 import SelectLyricsSection from "./select-lyrics-section";
+import ShowLyricsSection from "./show-lyrics-section";
+
+type Section =
+  | { type: "CHOOSE_LYRICS" }
+  | { type: "SHOW_LYRICS"; artistId: number; trackId: number };
 
 const Selection: React.FC = () => {
+  const [section, setSection] = React.useState<Section>({
+    type: "CHOOSE_LYRICS",
+  });
+
+  const handleTrackSubmit = React.useCallback(
+    (artistId: number, trackId: number) => {
+      setSection({ type: "SHOW_LYRICS", artistId, trackId });
+    },
+    []
+  );
+
   return (
     <>
       <Title>Track Selection</Title>
-      <SelectLyricsSection
-        onSubmitLyricSelection={(artistId, trackId) =>
-          console.log({ artistId, trackId })
-        }
-      />
+      {section.type === "CHOOSE_LYRICS" ? (
+        <SelectLyricsSection onSubmitLyricSelection={handleTrackSubmit} />
+      ) : (
+        <ShowLyricsSection
+          artistId={section.artistId}
+          trackId={section.trackId}
+        />
+      )}
     </>
   );
 };
---
+++ src/pages/selection/show-lyrics-section.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface ShowLyricsSectionProps {
+  artistId: number;
+  trackId: number;
+}
+
+const ShowLyricsSection: React.FC<ShowLyricsSectionProps> = () => {
+  return (
+    <section>
+      <h2>TODO</h2>
+    </section>
+  );
+};
+
+export default ShowLyricsSection;
